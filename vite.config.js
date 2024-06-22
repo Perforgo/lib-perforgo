9648018c15de07ddd39409893821f009dc539bb7
Gavin Kemp <gpk@gavin-kemp.com>
2024-06-22 15:23:37 +0100

    Configure package.json and externalize deps

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,14 @@ export default defineConfig({
 			entry: [resolve(__dirname, "lib/core.js")],
 			name: "Perforgo",
 		},
+		rollupOptions: {
+			external: ["web-vitals", "uid", "@nuxt/kit"],
+			output: {
+				globals: {
+					"web-vitals": "webVitals",
+					uid: "uid",
+				},
+			},
+		},
 	},
 });
