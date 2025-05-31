44a2fb630129303c13b68cfb3d084a1fc7bc304c
Gavin Kemp <gavin@Gavins-iMac.local>
2025-05-31 12:27:26 +0100

    Externalise uid and webVitals for UMD CDN build

---
+++ vite.config.umd.js
@@ -0,0 +1,25 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: [resolve(__dirname, "lib/perforgo.js")],
+      name: "Perforgo",
+      formats: ["umd"],
+      fileName: () => "perforgo.umd.js",
+    },
+    rollupOptions: {
+      external: ["@nuxt/kit"],
+      output: {
+        globals: {
+          "web-vitals": "webVitals",
+          uid: "uid",
+        },
+      },
+    },
+    minify: true,
+    outDir: "dist/cdn",
+    emptyOutDir: false,
+  },
+});
