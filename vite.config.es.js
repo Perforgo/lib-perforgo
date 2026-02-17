14dc2bfa732a94c2740776a5e11ba44b0952ef04
Gavin Kemp <gpk@gavin-kemp.com>
2026-02-17 18:33:25 +0000

    Support older config & browsers by transpiling down to old versions of JS

--- vite.config.es.js
+++ vite.config.es.js
@@ -4,6 +4,7 @@ import dts from "vite-plugin-dts";
 
 export default defineConfig({
   build: {
+    target: "es2015",
     lib: {
       entry: [resolve(__dirname, "lib/perforgo.js")],
       name: "Perforgo",
